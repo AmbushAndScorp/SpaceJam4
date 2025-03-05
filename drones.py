0ec8f713a3e3c4beaafb91b3271a070549a023d8
AmbushAndScorp <amalbert2004@gmail.com>
2025-03-05 09:40:29 -0500

    Add files via upload

--- drones.py
+++ drones.py
@@ -8,8 +8,8 @@ class Drone(SphereCollideObject):
     # how many drones spawned
     droneCount = 0
     def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
-        super(Drone, self).__init__(loader, modelPath, parentNode, nodeName, posVec, scaleVec + 10)
-        self.modelNode = loader.loadModel(modelPath)
+        super(Drone, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0,0,0), 3)
+        #self.modelNode = loader.loadModel(modelPath)
         self.modelNode.reparentTo(parentNode)
         self.modelNode.setPos(posVec)
         self.modelNode.setScale(scaleVec)
